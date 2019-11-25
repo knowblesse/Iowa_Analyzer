b39349abac3971cb873fa0ee21477246c27cde0e
Ji Hoon Jeong <knowblesse@gmail.com>
2019-11-25 16:46:06 +0900

    Choice trend plot added

--- Main.py
+++ Main.py
@@ -62,17 +62,18 @@ OUTPUT :
 +-------------+------------------------+
 |      0      |          1-4           |
 +-------------+------------------------+
-| Final score | numA, numB, numC, numD |
+| Final Score | numA, numB, numC, numD |
 +-------------+------------------------+
 """
-OUTPUT = np.zeros((numFile,5)) # final score | numA numB numC numD | prob 'C'
+OUTPUT = np.zeros((numFile,6)) # final score | numA numB numC numD | prob 'C'
 for i in np.arange(numFile):
     tempArr = DataList[i,:,:]
     OUTPUT[i,0] = tempArr[-1,5] # total score
-    OUTPUT[i,1::]  = np.array([sum(tempArr[:,1] == ord('A')),
+    OUTPUT[i,1:5]  = np.array([sum(tempArr[:,1] == ord('A')),
                                sum(tempArr[:,1] == ord('B')),
                                sum(tempArr[:,1] == ord('C')),
                                sum(tempArr[:,1] == ord('D'))])
+    OUTPUT[i,5] = np.mean(tempArr[:,5])
 
 ################################
 #           PLOT     1         #
@@ -96,7 +97,7 @@ fig1.show()
 ################################
 #           PLOT     2         #
 ################################
-# Total Score
+# Choice Ratio
 fig2 = plt.figure(2)
 ax2 = fig2.subplots(1,1)
 ax2.set_title('Best Choice Ratio')
@@ -111,3 +112,33 @@ ax2.set_ylabel('Ratio')
 ax2.plot([0,numFile-1],[0.25,0.25],'r--')
 
 fig2.show()
+
+################################
+#           PLOT     3         #
+################################
+# Choice trend change
+fig3 = plt.figure(3)
+ax3 = fig3.subplots(2,3)
+ax3[0,0].set_title('Option : A')
+ax3[0,1].set_title('Option : B')
+ax3[1,0].set_title('Option : C')
+ax3[1,1].set_title('Option : D')
+ax3[0,2].set_title('Option : A&B')
+ax3[1,2].set_title('Option : C&D')
+
+# Populate data
+ax3[0,0].plot(np.sum(np.sum(DataList[:,:,1] == ord('A'),axis=0).reshape(10,10),axis=1)/(10*numFile))
+ax3[0,1].plot(np.sum(np.sum(DataList[:,:,1] == ord('B'),axis=0).reshape(10,10),axis=1)/(10*numFile))
+ax3[1,0].plot(np.sum(np.sum(DataList[:,:,1] == ord('C'),axis=0).reshape(10,10),axis=1)/(10*numFile))
+ax3[1,1].plot(np.sum(np.sum(DataList[:,:,1] == ord('D'),axis=0).reshape(10,10),axis=1)/(10*numFile))
+
+ax3[0,2].plot(np.sum(np.sum(DataList[:,:,1] <= ord('B'),axis=0).reshape(10,10),axis=1)/(10*numFile))
+ax3[1,2].plot(np.sum(np.sum(DataList[:,:,1] >= ord('C'),axis=0).reshape(10,10),axis=1)/(10*numFile))
+
+for i in range(2):
+    for j in range(3):
+        ax3[i,j].set_xlabel('Trials(*0.1)')
+        ax3[i,j].set_ylabel('Ratio')
+        ax3[i,j].set_ylim(0,1)
+
+fig3.show()
